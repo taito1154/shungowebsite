ad950b9c78bd82c36cb9acbfc680223e89b00f51
Taito <3835littletaitomonster@gmail.com>
2025-03-11 15:37:50 +0900

    クリック範囲がおかしい。

---
+++ src/app/works/DLEAGUE/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Header from "@/components/header";
+import Layout from "@/components/layout";
+export default function About() {
+  return (
+    <Layout>
+      <div className="flex flex-col items-center h-screen py-10 bg-slate-300">
+        <Header />
+        <h1 className="text-4xl font-bold text-black">DLEAGUE</h1>
+      </div>
+    </Layout>
+  );
+}
--- src/components/simplephotos.tsx
+++ src/components/simplephotos.tsx
@@ -1,47 +1,66 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Photos = () => {
   return (
-    <div className="flex flex-col space-y-36 sm:space-y-80 w-full items-center photo-container">
-      <div className="photo photo1 flex flex-col lg:flex-row ipad-pro:flex-col items-center gap-4 sm:gap-12 w-full bg-transparent md:pt-64">
-        <div className="relative w-[300px] sm:w-[500px] md:w-[400px] lg:w-[500px] h-[300px] sm:h-[500px] md:h-[400px] lg:h-[500px]">
+    <div className="pointer-events-none flex flex-col space-y-36 sm:space-y-80 w-full items-center photo-container">
+      <div className="photo photo1 flex flex-col lg:flex-row ipad-pro:flex-col items-center gap-4 sm:gap-12 w-full bg-transparent">
+        <Link
+          href="/works/Guilty"
+          className="relative w-[300px] sm:w-[500px] md:w-[400px] lg:w-[500px] h-[300px] sm:h-[500px] md:h-[400px] lg:h-[500px] transition-transform duration-500 hover:scale-105 pointer-events-auto"
+        >
           <Image
             src="/photo/Guilty1.jpg" // 画像のパスを指定
             alt="Photo1"
-            // width={500}
-            // height={500}
             fill
-            className="object-contain
+            className="
+            Guilty
+            object-contain
           rounded-lg
           shadow-lg
           opacity-0
-          flex-shrink-0"
+          flex-shrink-0
+          "
           />
-        </div>
-        <div className="flex flex-col ml-0 lg:ml-[240px]  ipad-pro:ml-0">
-          <p className="description1 text-3xl sm:text-8xl text-left opacity-0 text-white">
+        </Link>
+
+        <div className="flex flex-col ml-0 lg:ml-[240px]  ipad-pro:ml-0 group">
+          <Link
+            href="/works/Guilty"
+            className="description1 text-3xl sm:text-8xl text-left opacity-0 text-white group-hover:text-gray-400"
+          >
             Guilty
-          </p>
-          <p className="description1 text-2xl sm:text-6xl text-left opacity-0 text-white">
+          </Link>
+          <Link
+            href="/works/Guilty"
+            className="description1 text-2xl sm:text-6xl text-left opacity-0 text-white group-hover:text-gray-400"
+          >
             BE:FIRST
-          </p>
+          </Link>
         </div>
       </div>
-      <div className="photo photo2 flex flex-col-reverse lg:flex-row ipad-pro:flex-col-reverse items-center gap-4 sm:gap-8 w-full  bg-transparent ">
-        <div className="flex flex-col ml-0 lg:ml-[150px] ipad-pro:ml-0">
-          <p className="description2 text-3xl sm:text-8xl  text-left opacity-0 text-white w-full">
+      <div className="photo photo2 flex flex-col-reverse lg:flex-row ipad-pro:flex-col-reverse items-center gap-4 sm:gap-8 w-full  bg-transparent">
+        <div className="flex flex-col ml-0 lg:ml-[150px] ipad-pro:ml-0 group">
+          <Link
+            href="/works/DLEAGUE"
+            className="description2 text-3xl sm:text-8xl  text-left opacity-0 text-white w-full group-hover:text-gray-400"
+          >
             DLEAGUE
-          </p>
-          <p className="description2 text-2xl sm:text-6xl text-left opacity-0 text-white w-full ">
+          </Link>
+          <Link
+            href="/works/DLEAGUE"
+            className="description2 text-2xl sm:text-6xl text-left opacity-0 text-white w-full group-hover:text-gray-400"
+          >
             Avex Royal Brats
-          </p>
+          </Link>
         </div>
-        <div className="relative w-[300px] sm:w-[500px] md:w-[400px] lg:w-[500px] h-[300px] sm:h-[500px] md:h-[400px] lg:h-[500px]">
+        <Link
+          href="/works/DLEAGUE"
+          className="relative w-[300px] sm:w-[500px] md:w-[400px] lg:w-[500px] h-[300px] sm:h-[500px] md:h-[400px] lg:h-[500px] transition-transform duration-500 hover:scale-105"
+        >
           <Image
             src="/photo/ARB.jpg" // 画像のパスを指定
             alt="Photo2"
-            // width={500}
-            // height={500}
             fill
             className="
           rounded-lg
@@ -51,27 +70,34 @@ const Photos = () => {
           object-contain
           bg-white"
           />
-        </div>
+        </Link>
       </div>
       <div className="photo photo3 flex flex-col lg:flex-row ipad-pro:flex-col items-center gap-4 sm:gap-12 w-full bg-transparent ">
-        <div className="relative w-[300px] sm:w-[500px] md:w-[400px] lg:w-[500px] h-[300px] sm:h-[500px] md:h-[400px] lg:h-[500px]">
+        <Link
+          href="/works/Iwonder"
+          className="relative w-[300px] sm:w-[500px] md:w-[400px] lg:w-[500px] h-[300px] sm:h-[500px] md:h-[400px] lg:h-[500px] transition-transform duration-500 hover:scale-105"
+        >
           <Image
             src="/photo/IWONDER.jpg"
             alt="Photo3"
-            // width={500}
-            // height={500}
             fill
             className="object-contain rounded-lg shadow-lg opacity-0 flex-shrink-0"
           />
-        </div>
+        </Link>
         {/* 説明文をラップ */}
-        <div className="flex flex-col ml-0 lg:ml-[240px]  ipad-pro:ml-0">
-          <p className="description3 text-3xl sm:text-8xl text-left opacity-0 text-white">
+        <div className="flex flex-col ml-0 lg:ml-[240px]  ipad-pro:ml-0 group ">
+          <Link
+            href="/works/Iwonder"
+            className="description3 text-3xl sm:text-8xl text-left opacity-0 text-white group-hover:text-gray-400"
+          >
             IWONDER
-          </p>
-          <p className="description3 text-2xl sm:text-6xl text-left opacity-0 text-white">
+          </Link>
+          <Link
+            href="/works/Iwonder"
+            className="description3 text-2xl sm:text-6xl text-left opacity-0 text-white group-hover:text-gray-400"
+          >
             Daice
-          </p>
+          </Link>
         </div>
       </div>
 
