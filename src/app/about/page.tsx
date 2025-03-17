b4a3fe74a517c0318f9cde1d476d2a6f90c9ee0b
Taito <3835littletaitomonster@gmail.com>
2025-03-17 19:16:59 +0900

    aboutも完成

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,47 +6,54 @@ export default function About() {
   return (
     <Layout>
       <Background />
-      <div className="flex flex-col items-center min-h-screen py-24 bg-transparent">
+      {/* 全体を max-w-4xl に制限して中央寄せ */}
+      <div className="w-full max-w-[21rem] sm:max-w-md md:max-w-2xl lg:max-w-4xl mx-auto flex flex-col items-center min-h-screen py-24 bg-transparent">
         {/* 宣材写真 */}
-        <div className="mb-8">
+        <div className="mb-16 w-[300px] sm:w-[380px] md:w-[450px] lg:w-[450px]">
           <Image
             src="/photo/shungoartist.png"
             alt="宣材写真"
-            width={400}
-            height={400}
+            layout="responsive"
+            width={500} // アスペクト比を保つための値
+            height={500} // アスペクト比を保つための値
             className="rounded-full"
           />
         </div>
 
-        <div className="mb-8 p-8 bg-gray-800 rounded-lg text-white text-center max-w-4xl shadow-lg">
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-            <div className="flex flex-col items-center">
-              <p className="font-bold text-xl sm:text-3xl">身長</p>
-              <p className="text-xl sm:text-3xl">174cm</p>
+        {/* 基本情報セクション */}
+        <div className="title-font mb-8 w-full">
+          <div className="grid grid-cols-2 gap-6">
+            <div className="border-b border-white pb-2">
+              <p className="font-bold text-xl sm:text-3xl md:text-5xl">身長</p>
+              <p className="text-xl sm:text-3xl md:text-5xl">174cm</p>
             </div>
-            <div className="flex flex-col items-center">
-              <p className="font-bold text-xl sm:text-3xl">体重</p>
-              <p className="text-xl sm:text-3xl">78kg</p>
+            <div className="border-b border-white pb-2">
+              <p className="font-bold text-xl sm:text-3xl md:text-5xl">体重</p>
+              <p className="text-xl sm:text-3xl md:text-5xl">78kg</p>
             </div>
-            <div className="flex flex-col items-center">
-              <p className="font-bold text-xl sm:text-3xl">生年月日</p>
-              <p className="text-xl sm:text-3xl">2001年2月28日</p>
+            <div className="border-b border-white pb-2">
+              <p className="font-bold text-xl sm:text-3xl md:text-5xl">
+                生年月日
+              </p>
+              <p className="text-xl sm:text-3xl md:text-5xl">2001年2月28日</p>
             </div>
-            <div className="flex flex-col items-center">
-              <p className="font-bold text-xl sm:text-3xl">出身</p>
-              <p className="text-xl sm:text-3xl">福岡</p>
+            <div className="border-b border-white pb-2">
+              <p className="font-bold text-xl sm:text-3xl md:text-5xl">出身</p>
+              <p className="text-xl sm:text-3xl md:text-5xl">福岡</p>
             </div>
-            <div className="sm:col-span-2 flex flex-col items-center">
-              <p className="font-bold text-xl sm:text-3xl">趣味</p>
-              <p className="text-xl sm:text-3xl">ファッション</p>
+            <div className="border-b border-white pb-2 sm:col-span-2">
+              <p className="font-bold text-xl sm:text-3xl md:text-5xl">趣味</p>
+              <p className="text-xl sm:text-3xl md:text-5xl">ファッション</p>
             </div>
           </div>
         </div>
 
         {/* 自己紹介文 */}
-        <div className="mb-8 w-full max-w-2xl text-white">
-          <h2 className="text-2xl font-bold mb-4">self-introduction</h2>
-          <p>
+        <div className="mb-8 w-full  text-white">
+          <h2 className="text-2xl font-bold mb-4 md:text-5xl">
+            self-introduction
+          </h2>
+          <p className="md:text-3xl">
             2001年生まれ、福岡県出身。 21-22 SEASONのavex
             ROYALBRATSオーディションを勝ち抜き、福岡から上京。
             海外のスタイルを得意とするも、ブレイキンをやっていたりと様々なジャンルを学び、スキルを磨いてきた。
@@ -58,9 +65,9 @@ export default function About() {
         </div>
 
         {/* 実績セクション */}
-        <div className="mb-8 w-full max-w-2xl text-white">
-          <h2 className="text-2xl font-bold mb-4">achievement</h2>
-          <ul className="list-disc ml-6">
+        <div className="mb-8 w-full text-white">
+          <h2 className="text-2xl font-bold mb-4 md:text-5xl">achievement</h2>
+          <ul className="list-disc ml-6 md:text-3xl">
             <li>実績A: 説明文...</li>
             <li>実績B: 説明文...</li>
             <li>実績C: 説明文...</li>
